1b01cd8c71aaa233fa1130b007e2119c41d3505c
Anne Sophie T. Nguyen <anne.sophie.t.nguyen@avanade.com>
2018-05-17 15:38:20 -0700

    Created test table and data. Need to load data via ngrx.

---
+++ src/app/data/dummy-data.ts
@@ -0,0 +1,35 @@
+export const STAFF_DATA: any[] =
+[
+  {
+    description: 'Test 1',
+    code: 10001,
+    role: 'AGENT',
+		active: 'N',
+		modifiedBy: 'SU',
+		modifiedDate: 'May 17, 2018 3:00:00 PM PDT'
+  },
+  {
+    description: 'Test 2',
+    code: 10002,
+    role: 'MD',
+		active: 'Y',
+		modifiedBy: 'S603',
+		modifiedDate: 'May 1, 2018 01:00:00 AM PDT'
+  },
+  {
+    description: 'Tester',
+    code: 10003,
+    role: 'RN',
+		active: 'Y',
+		modifiedBy: 'R363',
+		modifiedDate: 'May 11, 2018 7:00:00 PM PDT'
+  },
+  {
+    description: 'Testee',
+    code: 10004,
+    role: 'AGENT',
+		active: 'Y',
+		modifiedBy: 'L003',
+		modifiedDate: 'May 41, 2018 9:00:00 AM PDT'
+  },
+]

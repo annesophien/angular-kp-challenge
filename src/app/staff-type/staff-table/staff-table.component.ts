ed0ccc9816754143de2c60c5855693fecdfc44c5
Anne Sophie T. Nguyen <anne.sophie.t.nguyen@avanade.com>
2018-05-17 19:52:29 -0700

    Row highlighting.

--- src/app/staff-type/staff-table/staff-table.component.ts
+++ src/app/staff-type/staff-table/staff-table.component.ts
@@ -7,10 +7,19 @@ import { StaffService } from '../../staff.service';
   styleUrls: ['./staff-table.component.css']
 })
 export class StaffTableComponent implements OnInit {
+	selectedRow : Number;
 
   constructor(private staffService: StaffService) { }
 
   ngOnInit() {
-  }
+	}
+	
+	onEdit() {
+
+	}
+
+	setClickedRow(index) {
+		this.selectedRow = index;
+	}
 
 }
